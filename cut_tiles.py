3d9dd3e51d45a8c5273efc5cba52726628d9bc16
Christopher Groskopf <staringmonkey@gmail.com>
2013-07-09 09:50:53 -0500

    Backport from bob's concert tickets.

---
+++ cut_tiles.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+
+from PIL import Image
+
+TILE_WIDTH = 256
+TILE_HEIGHT = 256
+MIN_ZOOM = 0 
+MAX_ZOOM = 4
+
+# Open original image
+full_image = Image.open('www/img/okkervil.lpjacket-html.jpg')
+full_width, full_height = full_image.size
+
+zoom = MAX_ZOOM 
+
+while zoom >= MIN_ZOOM:
+    # Max zoom level is pixel-perfect
+    if zoom == MAX_ZOOM:
+        image = full_image
+
+        width = full_width
+        height = full_height
+    # Other zoom levels are downsampled
+    else:
+        width = full_width / (2 ** (MAX_ZOOM - zoom))
+        height = full_height / (2 ** (MAX_ZOOM - zoom))
+
+        image = full_image.resize((width, height), Image.ANTIALIAS)
+
+    max_x = width + 256 - (width % TILE_WIDTH)
+    max_y = height + 256 - (height % TILE_HEIGHT) 
+    
+    x = 0
+
+    while x < max_x:
+        tile_x = x / TILE_WIDTH 
+        
+        y = 0
+        
+        while y < max_y:
+            tile_y = y / TILE_HEIGHT
+
+            tile = image.crop((x, y, x + TILE_WIDTH, y + TILE_HEIGHT))
+
+            folder = 'www/img/tiles/%i/%i/' % (zoom, tile_x)
+
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            path = os.path.join(folder, '%i.jpg' % tile_y)
+
+            print path
+
+            tile.save(path, 'JPEG')
+
+            y += TILE_HEIGHT
+        
+        x += TILE_WIDTH
+
+    zoom -= 1
--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,74 @@
 $(function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4)
+    var $nav = $('#nav');
+    var $topper = $('#topper');
+
+    var MAX_X = 8550;
+    var MAX_Y = 5768;
+    var MIN_ZOOM = 0;
+    var MAX_ZOOM = 4;
+    var COORDINATE_MULTIPLIER = 1 / Math.pow(2, MAX_ZOOM - MIN_ZOOM);
+
+    function xy(x, y) {
+        /*
+         * Convert image-space pixel coords into map-space pseudo-lat-lng coords.
+         */
+        return new L.LatLng(-y * COORDINATE_MULTIPLIER, x * COORDINATE_MULTIPLIER);
+    }
+
+    var MIN_COORDS = new L.LatLng(0, 0);
+    var CENTER_COORDS = xy(4275, 2884);
+    var MAX_COORDS = xy(MAX_X, MAX_Y); 
+
+    var superzoom = L.map('superzoom', {
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        crs: L.CRS.Simple,
+        zoomControl: false,
+        attributionControl: false
     });
 
-    var html = JST.example(context);
+    var zoom_control = new L.Control.Zoom({
+        position: 'topright'
+    }).addTo(superzoom);
 
-    $('#template-example').html(html);
+    //var tiles = L.tileLayer('/img/tiles/{z}/{x}/{y}.jpg', {
+    var tiles = L.tileLayer('http://{s}.npr.org/bob-boilens-wristbands-2012/img/tiles/{z}/{x}/{y}.jpg', {
+        subdomains: ['apps', 'apps2'],
+        continuousWorld: true,
+        noWrap: true
+    }).addTo(superzoom);
+
+    function recalculate_map_offset() {
+        /*
+         * Calculates an appropriate map offset to compensate for the header.
+         */
+        var header_height = $nav.height();
+        
+        if ($topper.is(':visible')) {
+            header_height += $topper.height();
+        }
+
+        var offset = superzoom.unproject(new L.Point(0, -header_height), superzoom.getZoom());
+        superzoom.setMaxBounds(new L.LatLngBounds(offset, MAX_COORDS));
+    }
+
+    superzoom.on('load', recalculate_map_offset);
+    superzoom.on('zoomend', recalculate_map_offset);
+    $(window).resize(recalculate_map_offset);
+
+    $('#about').click(function(){
+        if($('.modal-body').children().length < 1 ) {
+            $('.modal h3').html($('.legend-contents .headline').html());
+            $('.legend-contents').clone().appendTo('.modal-body');
+            $('.modal-body .legend-contents .headline').hide();
+        }
+    });
+
+    window.superzoom_to = function(x, y, zoom) {
+        superzoom.setView(xy(x, y), zoom);
+        $('.modal').modal('hide');
+    }
+
+    // Load!
+    superzoom.setView(CENTER_COORDS, MIN_ZOOM);
 });
